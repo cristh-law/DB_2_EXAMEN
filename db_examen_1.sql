7872b3d3561e49ec084838bd64346a0ac53a9cd3
cristh-law <cristhianpaullauravargas90@gmail.com>
2024-10-06 18:17:34 -0500

    Add files via upload

---
+++ db_examen_1.sql
@@ -0,0 +1,14 @@
+-- Database: db_supermercado_DDL
+
+-- DROP DATABASE IF EXISTS "db_supermercado_DDL";
+
+CREATE DATABASE "db_supermercado_DDL"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Peru.1252'
+    LC_CTYPE = 'Spanish_Peru.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
